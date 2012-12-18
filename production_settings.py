64d3846d31518c812bcd271e2f9d2d88d45f06e9
Eric Roberts <eric@entelechynt.com>
2012-12-18 16:45:38 -0500

    Added initial production settings and templates

---
+++ production_settings.py
@@ -0,0 +1,29 @@
+from settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/usr/local/django/lifeleft/static',
+)
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    '/usr/local/django/lifeleft/templates'
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'lifeexpectancy',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      
+        'PASSWORD': '',                  #
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
