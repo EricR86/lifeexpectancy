949bac6a979f89e76fb843b52cd1774c0082c16d
Eric Roberts <eric@entelechynt.com>
2012-12-19 16:53:36 -0500

    Updated production settings

--- production_settings.py
+++ production_settings.py
@@ -21,8 +21,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'lifeexpectancy',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      
-        'PASSWORD': '',                  #
+        'USER': 'django_login',
+        'PASSWORD': 'lol1django2databases',
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
